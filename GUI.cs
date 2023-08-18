adeef6f297d2277f9ff09a97df6a5a1ffb5bc58c
karl-aleksander <KarlTheFish@users.noreply.github.com>
2023-08-18 14:35:47 +0300

    Fixed button bug, starting to actually create a layout

--- GUI.cs
+++ GUI.cs
@@ -1,4 +1,5 @@
 using Gdk;
+using GLib;
 
 #pragma warning disable CS0612 // Type or member is obsolete
 namespace Angelwave;
@@ -13,85 +14,70 @@ public class GUI
         Application.Init();
         
         //Create a new window
-        Window window = new Window("Baby's First GTK# Application!");
+        Window window = new Window("AngelWave");
         window.Resize(400, 400);
 
-        window.DeleteEvent += delegate(object o, DeleteEventArgs args) {
+        window.DeleteEvent += delegate(object o, DeleteEventArgs args) { //Event called when window is closed
             args.RetVal = true;
-            Window confirm = new Window("Confirmation");
+            var confirm = new Window("Confirmation");
             confirm.Resize(200, 100);
-            VBox cont3 = new VBox();
-            HBox buttons = new HBox();
-            Button yes = new Button("Yes");
-            Button no = new Button("No");
-            Label sure = new Label("Are you sure?");
+            var cont3 = new VBox();
+            var buttons = new HBox();
+            var yes = new Button("Yes");
+            var no = new Button("No");
+            var sure = new Label("Are you sure?");
             buttons.Add(yes);
             buttons.Add(no);
             cont3.Add(sure);
             cont3.Add(buttons);
-            yes.ButtonPressEvent += new ButtonPressEventHandler(ExitHandler);
-            no.ButtonPressEvent += new ButtonPressEventHandler(NoHandler);
+            yes.ButtonPressEvent += ExitHandler;
+            no.ButtonPressEvent += NoHandler;
             confirm.Add(cont3);
             confirm.ShowAll();
-            
-            [GLib.ConnectBefore]
-            void ExitHandler(object obj, ButtonPressEventArgs args2){
-                if (args2.Event.Type == EventType.ButtonPress){
+
+            [ConnectBefore]
+            void ExitHandler(object obj, ButtonPressEventArgs args2)
+            {
+                if (args2.Event.Button == 1)
+                {
                     args.RetVal = false;
                     Application.Quit();
                 }
             }
 
-            [GLib.ConnectBefore]
-            void NoHandler(object obj, ButtonPressEventArgs args2){
-                if (args2.Event.Type == EventType.ButtonPress){
-                    confirm.Destroy();
-                }
+            [ConnectBefore]
+            void NoHandler(object obj, ButtonPressEventArgs args2)
+            {
+                if (args2.Event.Button == 1) confirm.Destroy();
             }
         };
-
-        VBox cont2 = new VBox(false, 2);
-
-        ButtonBox bBox = new ButtonBox(Orientation.Horizontal);
-
-        Button finder2 = new Button("Find Songs");
         
-        Button button3 = new Button("Random Button");
-        bBox.Add(finder2); bBox.Add(button3);
+        Table layout = new Table(3, 3, false);
+        
+        //Creating the buttons
+        
+        Button find = new Button("Find");
 
-        bBox.Margin = 10;
-
-        button3.ButtonPressEvent += new ButtonPressEventHandler(ButtonPressHandler);
+        //Attaching functions to the buttons
         
-        //Create a label with some text in it
-        Label label = new Label();
-        label.Text = "Hello World!";
+        find.ButtonPressEvent += FindHandler;
         
-        cont2.Add(label);
-        cont2.Add(bBox);
+        //Adding the buttons to the layout
+        layout.Attach(find, 0, 1, 0, 1);
         
-        //Add label to the window
-        window.Add(cont2);
         
+        window.Add(layout);
         window.ShowAll();
         
         Application.Run();
     }
     
-    static void AddButton(HBox box, string givenName){
-        box.PackStart(new Button (givenName), false, true, 2);
-    }
-
-    [GLib.ConnectBefore] //Don't know what it actually does, but event doesn't work properly without it
-    static void ButtonPressHandler(object obj, ButtonPressEventArgs args){
-        if (args.Event.Type == EventType.ButtonPress){
-            Window pressWindow = new Window("Button pressed");
-            pressWindow.Resize(200, 200);
-            Label lbl = new Label("Hello World!");
-            pressWindow.Add(lbl);
-            pressWindow.ShowNow();
+    [ConnectBefore]
+    static void FindHandler(object obj, ButtonPressEventArgs args) {
+        if (args.Event.Button == 1) {
+            Console.WriteLine("Find button pressed");
         }
-        
-        
     }
 }
+
+
