7de22fd4257bf0a4088083cbadad025fa89866fb
karl-aleksander <KarlTheFish@users.noreply.github.com>
2023-09-05 18:18:37 +0300

    Starting remodeling the app, moving to Avalonia UI

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace Angelwave.Views {
+    
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
